89be2db8e1a49672b1467a4953a1da18bd3138ae
Adam <adam.e.sheaffer@gmail.com>
2019-12-08 11:54:17 -0600

    default port 5000

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const express = require("express");
 const bodyParser = require("body-parser");
 const low = require("lowdb");
